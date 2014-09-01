1d871517c4e77faae55296134b0db02f5ac1f7c9
Pntbr <pntbr@scopyleft.fr>
2014-09-01 11:56:12 +0200

    first commit

---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+  summary: "A module to share contents, articles, datas, posts for multi BàO",
+  version: "0.0.1",
+  git: "https://github.com/scopyleft/multibao-contribution"
+});
+
+Package.onUse(function (api, where) {
+  api.versionsFrom('METEOR@0.9.0.1');
+});
